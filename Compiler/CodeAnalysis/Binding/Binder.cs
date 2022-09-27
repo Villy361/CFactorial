d5cb3425dcd569bac05b5cea91592f9b3af40968
Vily361 <v.pearl.dev@gmail.com>
2022-09-27 17:32:04 -0400

    Update Binder.cs

--- Compiler/CodeAnalysis/Binding/Binder.cs
+++ Compiler/CodeAnalysis/Binding/Binder.cs
@@ -19,6 +19,8 @@ namespace CFactorial.CodeAnalysis.Binding
                     return BindUnaryExpression((UnaryExpressionSyntax)syntax);
                 case SyntaxKind.LiteralExpression:
                     return BindLiteralExpression((LiteralExpressionSyntax)syntax);
+                case SyntaxKind.ParenthesizedExpression:
+                    return BindExpression(((ParenthesizedExpressionSyntax)syntax).Expression);
                 default:
                     throw new Exception($"Unexpected syntax {syntax.Kind}");
             }
